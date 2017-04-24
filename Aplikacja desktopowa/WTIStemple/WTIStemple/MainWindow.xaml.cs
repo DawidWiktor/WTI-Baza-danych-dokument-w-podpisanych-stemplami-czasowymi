e170991cf46870246f323d6029494cd26e711fcd
damifil <damifil15@gmail.com>
2017-04-24 17:30:58 +0200

    stworzenie projektu i widokow
    brakuje widoku z archiwum

---
+++ Aplikacja desktopowa/WTIStemple/WTIStemple/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WTIStemple
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            register wnd = new register();
+            wnd.Owner = this;
+            wnd.Show();
+            this.Hide();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            main wnd2 = new main();
+          
+            wnd2.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Aplikacja desktopowa/WTIStemple/WTIStemple/main.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WTIStemple
+{
+    /// <summary>
+    /// Logika interakcji dla klasy main.xaml
+    /// </summary>
+    public partial class main : Window
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chekfileControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void archiveControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void settingsControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
